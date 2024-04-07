5a733a49411b94b2a53c71cd6ec5a6444848a721
jay16y <202301215@daiict.ac.in>
2024-04-07 18:49:24 +0530

    remove int main for total change

--- Main_code.cpp
+++ Main_code.cpp
@@ -258,25 +258,3 @@ public:
 };
 
 
-
-int main() 
-{
-    int N, M, P;
-    cout << "Enter gate number: ";
-    cin >> N;
-
-    cout << "Enter capacity: ";
-    cin >> M;
-    if(M>150000)
-    cout<<"Overflow"<<endl;
-
-    cout << "Enter time (in minutes) for a single attendee to enter any gate: ";
-    cin >> P;
-
-    Stadium stadium(N, M, P);
-    stadium.Random_Assignment();
-    stadium.Calculate_waiting_time();
-    stadium.add_in_suggested_queue();
-
- return 0;
-}
