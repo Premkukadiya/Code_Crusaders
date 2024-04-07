90bf5f55938ecfe89310f0cf1c43c7ba889a579c
Arav <aravvaitha365@gmail.com>
2024-04-07 19:07:08 +0530

    Finishing Project

--- Main_code.cpp
+++ Main_code.cpp
@@ -10,35 +10,36 @@ using namespace std;
 class Stadium
 {
 private:
-    vector<queue<int>> gate;         
-    vector<queue<int>> VIP_gate; 
-    vector<float> waiting_time;  
-    int N;                        // Number of gates in the stadium
-    int M;                        // Total capacity of the stadium
-    float P;                      // Time taken (in min) for a single attendee to enter any gate
-    int sum;                      // Total attendee at gate
-    int NUM_GATE_VIP;             
-    int NUM_VIP;                  
-    float TIME_VIP;               // Entry time for VIP attendees
-    int j;                        
+    vector<queue<int>> gate;
+    vector<queue<int>> VIP_gate;
+    vector<float> waiting_time;
+    int N;   // Number of gates in the stadium
+    int M;   // Total capacity of the stadium
+    float P; // Time taken (in min) for a single attendee to enter any gate
+    int sum; // Total attendee at gate
+    int NUM_GATE_VIP;
+    int NUM_VIP;
+    float TIME_VIP; // Entry time for VIP attendees
+    int j;
 
 public:
     // Constructor to initialize the stadium
     Stadium(int numGates, int capacity, float entryTime, int GATE_VIP, int TOTAL_VIP_PEOPLE, float time) : N(numGates), M(capacity), P(entryTime), NUM_GATE_VIP(GATE_VIP), NUM_VIP(TOTAL_VIP_PEOPLE), TIME_VIP(time)
     {
-        gate.resize(N);         
-        waiting_time.resize(N); 
-        
+        gate.resize(N);
+        waiting_time.resize(N);
+
         if (NUM_GATE_VIP > 0 && NUM_GATE_VIP <= N)
         {
             VIP_gate.resize(NUM_GATE_VIP);
         }
     }
-     // Function to assign gate for VIPs
+
+    // Function to assign gate for VIPs
     void Random_Assignment_For_VIP()
     {
         cout << "Assigning  VIP Attendees..." << endl;
-        srand(time(NULL)); 
+        srand(time(NULL));
         int j = NUM_VIP;
         int randgate;
         int newpeople;
@@ -49,32 +50,31 @@ public:
             else
                 randgate = rand() % NUM_GATE_VIP;
 
-            newpeople = rand() % 20 + 1; 
+            newpeople = rand() % 20 + 1;
             if (j < newpeople)
                 newpeople = j;
 
             for (int i = 0; i < newpeople; i++)
             {
-                VIP_gate[randgate].push(i); 
+                VIP_gate[randgate].push(i);
             }
             j -= newpeople;
         }
 
-        
         for (int i = 0; i < NUM_GATE_VIP; i++)
         {
             cout << "Number of people at VIP gate " << i + 1 << " is " << VIP_gate[i].size() << endl;
         }
 
-        cout << "Waiting time for vip at each gate:"<<endl;
+        cout << "Waiting time for vip at each gate:" << endl;
         // Printing waiting time for VIPs
-        for(int i=0;i<NUM_GATE_VIP;i++)
+        for (int i = 0; i < NUM_GATE_VIP; i++)
         {
-            cout <<"Waiting time at gate " << i+1 << " is "<<(VIP_gate[i].size())*TIME_VIP<<endl;
+            cout << "Waiting time at gate " << i + 1 << " is " << (VIP_gate[i].size()) * TIME_VIP << endl;
         }
     }
 
-    //Function to claculate waiting time for general gates 
+    // Function to claculate waiting time for general gates
     void Calculate_waiting_time()
     {
         cout << "Waiting time for each gate:" << endl;
@@ -85,37 +85,24 @@ public:
         }
         cout << endl;
     }
-    // Waiting time of each gate 
-    void Waiting_time() 
+
+    // Suggesting Queue with min waiting time
+    int suggest_queue()
     {
-        for (int i = 0; i < N; ++i) {
-            waiting_time[i] = gate[i].size() * P;
-        }
-    }
-    // Assign first half of people 
-    void Random_Assignment(){
-            srand(time(NULL));
-        for (int i = 0; i < M / 2; i++) {
-            int a = rand() % N;
-            gate[a].push(i);
-        }
-        for (int i = 0; i < N; i++) {
-            cout << "Number of attendee in " << i + 1 << " gate " << gate[i].size() << endl;
-        }
-        cout << endl;
-    }
-    // Suggesting Queue with min waiting time 
-    int suggest_queue() {
         int min = waiting_time[0];
-        cout << "Suggested gates for this group is: ";
-        for (int i = 0; i < N; ++i) {
-            if (min >= waiting_time[i]) {
+        cout << "Suggested gates for this group: ";
+        for (int i = 0; i < N; ++i)
+        {
+            if (min >= waiting_time[i])
+            {
                 min = waiting_time[i];
             }
         }
-        int j;
-        for (int i = 0; i < N; ++i) {
-            if (waiting_time[i] == min) {
+
+        for (int i = 0; i < N; ++i)
+        {
+            if (waiting_time[i] == min)
+            {
                 cout << i + 1 << " ";
                 j = i;
             }
@@ -124,7 +111,7 @@ public:
         return j;
     }
 
-    // Function to arrange gates according to waiting time 
+    // Function to arrange gates according to waiting time
     void sort()
     {
         int i = 0;
@@ -144,7 +131,6 @@ public:
     }
 
     // Function to arrage people (to minimize waiting time)
-    
     void Internal_Arrangement(int k)
     {
         cout << "After arranging people for minimum time :" << endl;
@@ -167,6 +153,7 @@ public:
             }
         }
     }
+
     // Total no of people in Queue
     int sum_of_the_number_of_people_in_gates()
     {
@@ -177,7 +164,8 @@ public:
         }
         return sum;
     }
-    // Assigning  half of people 
+
+    // Assigning  half of people
     void addition_of_people_in_the_queue()
     {
         cout << "Now adding a random number of people to random gates:" << endl;
@@ -187,7 +175,7 @@ public:
         else
             a = (M / 2) + 1;
         int z;
-        
+
         if (M / 2 < 200)
             z = 10;
         else if (M / 2 > 200 && M / 2 < 1000)
@@ -204,13 +192,13 @@ public:
         srand(time(nullptr));
         while (a != 0 && a > 0)
         {
-            int randomgate = rand() % N;      
-            int newpeople = (rand() % z) + 1; 
+            int randomgate = rand() % N;
+            int newpeople = (rand() % z) + 1;
             if (a < newpeople)
                 newpeople = a;
             for (int i = 0; i < newpeople; i++)
             {
-                gate[randomgate].push(i); 
+                gate[randomgate].push(i);
             }
             cout << newpeople << " people added at gate no " << randomgate + 1 << endl;
             k = sum_of_the_number_of_people_in_gates();
@@ -221,7 +209,33 @@ public:
             a -= newpeople;
         }
     }
-    // Function allowing user to enter their group size 
+
+    // Waiting time of each gate
+    void Waiting_time()
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            waiting_time[i] = gate[i].size() * P;
+        }
+    }
+
+    // Assign rest of people
+    void Random_Assignment()
+    {
+        cout << "Assigning M/2 people randomly to N gates " << endl;
+        srand(time(NULL));
+        for (int i = 0; i < M / 2; i++)
+        {
+            int a = rand() % N;
+            gate[a].push(i);
+        }
+        for (int i = 0; i < N; i++)
+        {
+            cout << "Number of attendees in gate " << i + 1 << ": " << gate[i].size() << endl;
+        }
+        cout << endl;
+    }
+    // Function allowing user to enter their group size
     void Group_System()
     {
         int L, f;
@@ -255,8 +269,6 @@ public:
 
         Calculate_waiting_time();
     }
-
-    
 };
 
 int main()
@@ -267,7 +279,6 @@ int main()
     cout << "Enter the number of gates in the stadium: ";
     cin >> N;
 
-    
     if (N <= 0)
     {
         cout << "Invalid number of gates. Please enter a positive integer." << endl;
@@ -280,7 +291,6 @@ int main()
     if (M > 150000)
         cout << "Not a valid capacity" << endl;
 
-    
     if (M <= 0)
     {
         cout << "Invalid capacity. Please enter a positive integer." << endl;
@@ -293,7 +303,6 @@ int main()
     cout << "How many gates are for VIP entry: ";
     cin >> GATE_VIP;
 
-    
     if (GATE_VIP < 0 || GATE_VIP > N)
     {
         cout << "Invalid number of VIP gates. Please enter a non-negative integer less than or equal to the number of gates." << endl;
@@ -308,14 +317,13 @@ int main()
 
     cout << endl;
 
-     
     Stadium stadium(N, M - Num_VIP, P, GATE_VIP, Num_VIP, VIP_TIME);
     stadium.Random_Assignment();
     stadium.Calculate_waiting_time();
     int Z;
     cout << "what systum you want to pick(1/2):" << endl;
     cout << "1.minimize time for all attendees" << endl;
-    cout << "2.group systum" << endl;
+    cout << "2.group system" << endl;
     cin >> Z;
     if (Z == 1)
         stadium.addition_of_people_in_the_queue();
@@ -326,4 +334,3 @@ int main()
 
     return 0;
 }
-
